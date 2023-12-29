e12989c84211960570cf3216ac301af7ed214a4f
Eduardo Henrique <newdamage1@live.com>
2023-12-28 23:44:57 -0300

    Adicionar arquivos de projeto.

---
+++ pooPokemonApp/Pokedex.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPokemonApp
+{
+    public class Pokedex
+    {
+
+		public Pokedex()
+		{
+			this.InicializaLista();
+		}
+
+
+		private List<Pokemon> pokemons;
+
+		public List<Pokemon> Pokemons
+		{
+			get { return pokemons; }
+			
+		}
+
+		private void InicializaLista()
+		{
+			this.pokemons = new List<Pokemon>();
+
+			Pokemon p = new Pokemon("Bulba","Sua desc");
+			this.pokemons.Add(p);
+
+			p = new Pokemon("Dudao", "esse mesmo");
+			this.pokemons.Add(p);
+
+            p = new Pokemon("Dudao1", "esse mesmo1");
+            this.pokemons.Add(p);
+
+            p = new Pokemon("Dudao2", "esse mesmo2");
+            this.pokemons.Add(p);
+
+            p = new Pokemon("Dudao3", "esse mesmo3");
+            this.pokemons.Add(p);
+        }
+	}
+}
---
+++ pooPokemonApp/Pokemon.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPokemonApp
+{
+    public class Pokemon
+    {
+
+		public Pokemon() 
+		{
+			this.Nome = "";
+			this.Descricao = "";
+		}
+        public Pokemon(String nome, String descricao)
+        {
+            this.Nome = nome;
+            this.Descricao = descricao;
+        }
+
+
+        private String nome;
+
+		public String Nome
+		{
+			get { return this.nome; }
+			set { 
+				String texto = value.ToUpper();
+				this.nome = texto; 
+			}
+		}
+
+		private String descricao;
+
+		public String Descricao
+		{
+			get { return this.descricao; }
+			set {
+				this.descricao = value.ToUpper();	 ; 
+			}
+		}
+
+		public void ExibirDadosPokemom()
+		{
+			Console.WriteLine("Nome do Pokemon: " + this.Nome);
+
+			Console.WriteLine("Descrição do Pokemon: " + this.Descricao);
+
+		}
+
+	}
+}
